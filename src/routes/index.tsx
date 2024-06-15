3ec410493794b9846395d91905b7cec8d761ca68
Igor <iigorfelipe@gmail.com>
2024-06-15 11:07:42 -0300

    🐛 add base route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import About from '../pages/about';
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/portfolio/" element={<Work />} />
       <Route path="/portfolio/work" element={<Work />} />
       <Route path="/portfolio/about" element={<About />} />
       <Route path="*" element={<Navigate to="/portfolio/work" />} />
