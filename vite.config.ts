56d018cd4c88c0d1456796e8e2ce8486a0683bcd
Igor <iigorfelipe@gmail.com>
2024-06-17 12:01:19 -0300

    ✨ handle 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve } from 'path'
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
